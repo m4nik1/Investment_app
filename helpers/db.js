7220e2f518547afd431815c2eda5efc9673c478c
Manik Soomro <maniksoomro@hotmail.com>
2020-08-22 20:27:06 -0400

    adding fetch db func

--- helpers/db.js
+++ helpers/db.js
@@ -20,3 +20,38 @@ export function init () {
     return promise;
 }
 
+export function insertInvestment (symbol, shares, price) {
+    const promise = new Promise((resolve, reject) => {
+        db2.transaction(tx => {
+            tx.executeSql(
+                `INSERT INTO Stocks (symbol, shares, price) VALUES (?, ?, ?);`,
+                [symbol, shares, price],
+                (_, result) => {
+                    resolve(result);
+                },
+                (_, err) => {
+                 reject(err)
+                }
+            );
+        });
+    });
+    return promise;
+}
+
+export const fetchInvestment = () => {
+    const promise = new Promise((resolve, reject) => {
+        db.transaction(tx => {
+            tx.executeSql(
+                'SELECT * FROM Stocks',
+                [],
+                (_, result) => {
+                    resolve(result);
+                },
+                (_, err) => {
+                 reject(err)
+                }
+            );
+        });
+    });
+    return promise;
+}
--- store/invest-reducer.js
+++ store/invest-reducer.js
@@ -1,4 +1,4 @@
-import { ADD_INVEST } from './invest-actions';
+import { ADD_INVEST, SET_INVEST } from './invest-actions';
 import invest from '../model/invest';
 
 const initialState = {
@@ -7,8 +7,12 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case SET_INVEST:
+      console.log('working')
+      return {
+        investments: action.investments.map(in => new invest(in.symbol))
+      }
     case ADD_INVEST:
-      console.log('testing')
       const newInvestment = new invest(new Date().toString(), action.investData.symbol, action.investData.shares, action.investData.price);
       return {
         investments: state.investments.concat(newInvestment)
